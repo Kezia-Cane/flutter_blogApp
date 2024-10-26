3dd874841a8d1fbffe90aee513a6d2a4053388e6
Kesh <59233228+Kezia-Cane@users.noreply.github.com>
2024-10-26 23:16:09 +0800

    added flutter bloc

--- lib/presentation/bloc/auth_bloc.dart
+++ lib/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,16 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:blog_app/domain/usercases/user_sign_up.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final UserSignUp _userSignUp;
+  AuthBloc({
+    required UserSignUp userSignup,
+  })  : _userSignUp = userSignup,
+        super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) {});
   }
 }
